0ec1459dda56b7672fbdce8c48cdf8ad52b405c8
Will Clarke <willc@popcreative.co.uk>
2023-02-20 14:09:12 +0000

    hero banner

--- wp-content/themes/will/page_builder/hero.php
+++ wp-content/themes/will/page_builder/hero.php
@@ -5,28 +5,69 @@ $intro     = get_sub_field('intro');
 $img       = get_sub_field('image');
 $colours   = get_sub_field('colours');
 $cta       = get_sub_field('cta');
-?>
 
+var_dump($cta);
+?>
 <div class="hero" 
-    style=""
+    style="
+    <?php if($colours['background_colour']): ?>
+        background:<?php echo $colours['background_colour'];?>;
+    <?php else: ?>
+        background: #c14f51;
+    <?php endif; ?>
+    "
 >  
     <div class="hero-content">
         <?php if($header): ?>
-            <h1><?php echo esc_html($header); ?></h1>
+            <h1 class=""
+            style="
+            <?php if ($colours['header_colour']): ?>
+                color:<?php echo $colours['header_colour'];?>
+            <?php endif;?>
+            ">
+                <?php echo esc_html($header); ?>
+            </h1>
         <?php else: ?>
-            <h1><?php the_title(); ?></h1>
+            <h1 class=""
+            style="
+            <?php if ($colours['header_colour']): ?>
+                color:<?php echo $colours['header_colour'];?>
+            <?php endif;?>
+            ">
+                <?php the_title(); ?>
+            </h1>
         <?php endif; ?>
 
         <?php if($intro): ?>
-            <span><?php echo esc_html($intro); ?></span>
+            <span class=""
+            style="
+            <?php if ($colours['intro_colour']): ?>
+                color:<?php echo $colours['intro_colour'];?>
+            <?php endif; ?>
+            ">
+                <?php echo esc_html($intro); ?>
+            </span>
         <?php endif; ?>
 
         <?php if($cta['link'] && $cta['text']): ?>
-            <a class=""
-            style=""
+            <a class="cta"
+            style="
+            <?php if($colours['cta_colour']): ?>
+                background:<?php echo $colours['cta_colour'];?>;
+            <?php endif; ?>
+
+            <?php if($colours['cta_text_colour']): ?>
+                color:<?php echo $colours['cta_text_colour'];?>;
+            <?php endif; ?>
+            "
             href="<?php echo esc_url($cta['link']);?>">
                 <?php echo esc_html($cta['text']);?>
             </a>
         <?php endif; ?>
     </div>
+    <?php if ($img): ?>
+        <div class="hero-image">
+            <img src="<?php echo esc_url($img); ?>">
+        </div>
+    <?php endif; ?>
 </div>
